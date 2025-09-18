56328fbffebcbc0245ee77b63a85f650604440aa
JaGenn <dzhan.dzhan.2000@mail.ru>
2025-09-18 21:36:59 +0300

    feat: Minio and Image uploading feature

--- src/main/java/com/example/tasklist/service/TaskService.java
+++ src/main/java/com/example/tasklist/service/TaskService.java
@@ -1,6 +1,7 @@
 package com.example.tasklist.service;
 
 import com.example.tasklist.domain.entity.task.Task;
+import com.example.tasklist.domain.entity.task.TaskImage;
 
 import java.util.List;
 
@@ -9,5 +10,7 @@ public interface TaskService extends CrudService<Task, Long> {
     List<Task> getAllByUserId(Long userId);
 
     Task create(Task task, Long userId);
+
+    void uploadImage(Long id, TaskImage image);
 
 }
