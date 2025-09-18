0eb8250926f69af3ffd1ce4ee47fed2f3c19013c
JaGenn <dzhan.dzhan.2000@mail.ru>
2025-09-18 21:37:38 +0300

    redis annotation

--- src/main/java/com/example/tasklist/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/tasklist/service/impl/UserServiceImpl.java
@@ -7,6 +7,10 @@ import com.example.tasklist.domain.exception.ResourceNotFoundException;
 import com.example.tasklist.repository.UserRepository;
 import com.example.tasklist.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,33 +24,52 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+
     @Override
+    @Transactional(readOnly = true)
+    @Cacheable(value = "UserService::getByUsername", key = "#username")
     public User getByUsername(String username) {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
+    @Transactional(readOnly = true)
+    @Cacheable(value = "UserService::isTaskOwner", key = "#userId + '.' + #taskId")
     public boolean isTaskOwner(Long userId, Long taskId) {
         return userRepository.isTaskOwner(userId, taskId);
     }
 
     @Override
-    public User getById(Long userId) {
-        return userRepository.findById(userId)
+    @Transactional(readOnly = true)
+    @Cacheable(value = "UserService::getById", key = "#id")
+    public User getById(Long id) {
+        return userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
     @Transactional
+    @Caching(put = {
+            @CachePut(value = "UserService::getById", key = "#user.id"),
+            @CachePut(value = "UserService::getByUsername", key = "#user.username")
+    })
     public User update(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.update(user);
-        return user;
+        User existing = getById(user.getId());
+        existing.setName(user.getName());
+        existing.setUsername(user.getUsername());
+        existing.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(existing);
+        return existing;
     }
+
 
     @Override
     @Transactional
+    @Caching(cacheable = {
+            @Cacheable(value = "UserService::getById", key = "#user.id"),
+            @Cacheable(value = "UserService::getByUsername", key = "#user.username")
+    })
     public User create(User user) {
         if (userRepository.findByUsername(user.getUsername()).isPresent()) {
             throw new IllegalStateException("Username already exists.");
@@ -55,16 +78,16 @@ public class UserServiceImpl implements UserService {
             throw new IllegalStateException("Passwords do not match.");
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.create(user);
         Set<Role> roles = Set.of(Role.ROLE_USER);
-        userRepository.insertUserRole(user.getId(), Role.ROLE_USER);
         user.setRoles(roles);
+        userRepository.save(user);
         return user;
     }
 
     @Override
     @Transactional
-    public void delete(Long userId) {
-        userRepository.delete(userId);
+    @CacheEvict(value = "UserService::getById", key = "#id")
+    public void delete(Long id) {
+        userRepository.deleteById(id);
     }
 }
